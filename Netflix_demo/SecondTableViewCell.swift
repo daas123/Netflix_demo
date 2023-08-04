7a08a1a76c68a63e32aec77c6f16d68285d3e3aa
saad <saadneosoft@gmail.com>
2023-08-04 12:11:02 +0530

    basic second row

---
+++ Netflix_demo/SecondTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  SecondTableViewCell.swift
+//  Netflix_demo
+//
+//  Created by Neosoft on 04/08/23.
+//
+
+import UIKit
+
+class SecondTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var secondrowicon: UIImageView!
+    
+    @IBOutlet weak var secondcelllabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Netflix_demo/ViewController.swift
+++ Netflix_demo/ViewController.swift
@@ -39,23 +39,29 @@ extension ViewController : UITableViewDelegate , UITableViewDataSource {
     
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return 2
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if indexPath.row == 0{
-            let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath ) as! detailTableViewCell
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cell1", for: indexPath ) as! detailTableViewCell
             cell.imageview.image = UIImage(named: "icon")
             cell.name.text = "Saad Vadanagara"
             cell.downlocon.image = UIImage(systemName: "chevron.down")
             return cell
         }
+        else if indexPath.row == 1
+        {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cell2", for: indexPath ) as! SecondTableViewCell
+            cell.secondcelllabel.text = "Notifications"
+            cell.secondrowicon.image = UIImage(named: "bellicon")
+            return cell
+        }
         else
         {
-            let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath ) as! detailTableViewCell
-            cell.imageview.image = UIImage(systemName: "circle")
-            cell.name.text = "Saad Vadanagara"
-            cell.downlocon.image = UIImage(systemName: "chevron.down")
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cell2", for: indexPath ) as! SecondTableViewCell
+            cell.secondcelllabel.text = "hello"
+            cell.secondrowicon.image = UIImage(named: "bellicon")
             return cell
         }
         
@@ -64,9 +70,21 @@ extension ViewController : UITableViewDelegate , UITableViewDataSource {
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         if indexPath.row == 0
         {
-            return 200
+            return 180
         }
-        return 80
+        else if indexPath.row == 1
+        {
+            return 45
+        }
+        else if indexPath.row == 2
+        {
+            return 45
+        }
+        else 
+        {
+            return 45
+        }
+        
     }
     
 }
